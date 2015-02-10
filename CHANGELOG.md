c47d241ae7b6ae50f823e9764c5f49cd5eb01e86
Martin Zagora <zaggino@gmail.com>
2015-02-10 15:45:55 +1100

    added travis, changelog, contributing info, strings for localization and styles

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Changelog
+
+## 1.0.0 - 2015-??-??
+Version 1.0.0 is a complete rewrite of the old 0.x version.
+For the old changelog, see [CHANGELOG.md](https://github.com/zaggino/brackets-git/blob/master/CHANGELOG.md)
--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 /*global brackets, define, window*/
 
-define(function (require) {
+define(function (require, exports, module) {
     "use strict";
 
     // register es6 as javascript file extension in Brackets
@@ -8,7 +8,10 @@ define(function (require) {
     if (LanguageManager.getLanguageForExtension("es6") == null) {
         LanguageManager.getLanguageForExtension("js").addFileExtension("es6");
     }
-
+    
+    var ExtensionUtils = brackets.getModule("utils/ExtensionUtils");
+    ExtensionUtils.loadStyleSheet(module, "styles/main.less");
+    
     require("dist/browser-polyfill");
     require("dist/main")();
 
